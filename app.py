a5946570bb38c084eaece54a41e42de6b5ce3e1d
Dan Sigetti <sigettid@gmail.com>
2024-04-09 16:05:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ client = MongoClient(uri)
 db = client[DATABASE_NAME]
 collection = db['scan_results']
 
-#define the allowed_file function to allow all file types
+# Define the allowed_file function to allow all file types
 def allowed_file(filename):
     return True  # Allow all file types
 
-#function to scan uploaded file
+# Function to scan uploaded file
 def scan_uploaded_file(file_path, handle):
     try:
         result = amaas.grpc.scan_file(file_path, handle)
@@ -44,7 +44,7 @@ def scan_uploaded_file(file_path, handle):
 
 @app.route('/', methods=['GET'])
 def root():
-    #redirect to the login page when accessing the root URL
+    # Redirect to the login page when accessing the root URL
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -66,18 +66,18 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
 
-            #save the uploaded file
+            # Save the uploaded file
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
 
-            #initialize args with default values
+            # Initialize args with default values
             args = argparse.Namespace(
                 addr=os.environ.get('C1_ADDRESS'),
                 api_key=os.environ.get('C1_API_KEY'),
                 region=os.environ.get('C1_REGION')
             )
 
-            #initiate the AMaaS connection
+            # Initiate the AMaaS connection
             handle = amaas.grpc.init(args.addr, args.api_key, args.region)
 
             scan_result = scan_uploaded_file(file_path, handle)
@@ -90,7 +90,7 @@ def upload_file():
                     amaas.grpc.quit(handle)
                     return render_template('scan_results.html', scan_result_code=1, scan_results=scan_result_dict)
                 print("Scan result:", scan_result)
-                sys.stdout.flush()  #flush the stdout buffer
+                sys.stdout.flush()  # Flush the stdout buffer
             amaas.grpc.quit(handle)
 
             # Write scan result to MongoDB
@@ -98,6 +98,15 @@ def upload_file():
                 'filename': filename,
                 'scan_result': scan_result
             }
+
+            if scan_result is not None:
+                scan_result_dict = json.loads(scan_result)
+                scan_result_code = scan_result_dict.get('scanResult', -1)
+                result_entry['scan_result_code'] = scan_result_code
+            else:
+                # If scan result is None, indicate "Clean File"
+                result_entry['scan_result_code'] = "Clean File"
+
             collection.insert_one(result_entry)
 
             return render_template('scan_results.html', scan_message="File uploaded successfully.", scan_result_code=0)
